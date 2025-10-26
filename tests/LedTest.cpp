aa23afd2e9460d832ee0aec5093437890cc880f6
polame <polame.dev@gmail.com>
2025-10-26 11:42:15 +0300

    Сборка проекта

---
+++ tests/LedTest.cpp
@@ -0,0 +1,30 @@
+#include <lame/device/Led.h>
+
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTest/TestHarness.h>
+
+
+TEST_GROUP(EventTests) {
+
+LAME_Led led;
+
+void setup()
+{
+}
+
+void teardown()
+{
+}
+
+};
+
+TEST(EventTests, clearAfterCreate)
+{
+    CHECK_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+    printf("\nLed test");
+    return RUN_ALL_TESTS(argc, argv);
+}
